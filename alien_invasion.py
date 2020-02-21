138355810068607cf5e948bc4c5787280adf473c
Nathalie Pillen <56022155+NathaliePillen@users.noreply.github.com>
2020-02-21 14:41:12 +0100

    draw ship on the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,25 +3,28 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     #initialize pygame, settings, and screen object
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion") 
     
-    #Start the main loop
+    #make a ship
+    ship = Ship(screen)
+    
+    #start the main loop for the game
     while True:
-        
-        #watch for keyboard and mouse events.
+        #watch for keyboard and mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
                 
         #redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
                 
         #make the most recently drawn screen visible.
         pygame.display.flip()
--- ship.py
+++ ship.py
@@ -13,8 +13,8 @@ class Ship():
         self.screen_rect = screen.get_rect()
         
         #start each new ship at the bottom center of the screen
-        self.rect.centerx = self.screen.centerx
-        self.rect.bottom = self.screen.bottom
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
         
     def blitme(self):
         """Draw the ship at its current location"""
